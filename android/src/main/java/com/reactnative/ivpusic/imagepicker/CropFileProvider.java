d18e27f96a63e60445ab3b5440a54e842939f44c
huyefan <huyefan@baidu,com>
2021-03-01 16:47:36 +0800

    增加CropFileProvider文件

---
+++ android/src/main/java/com/reactnative/ivpusic/imagepicker/CropFileProvider.java
@@ -0,0 +1,7 @@
+package com.reactnative.ivpusic.imagepicker;
+
+import androidx.core.content.FileProvider;
+
+public class CropFileProvider extends FileProvider {
+    // This class intentionally left blank.
+}
